aa3625eaf68a865f8b2d27f0ffe01dce2ccbb265
Johan Jäderblom <johan.jaderblom@valtech.se>
2018-11-29 20:44:14 +0100

    Added cli

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,18 @@
-import 'package:adventofdartcode/adventofdartcode.dart' as adventofdartcode;
+import 'dart:io';
 
 main(List<String> arguments) {
-  print('Hello world: ${adventofdartcode.calculate()}!');
+  stdout.writeln('Choose day');
+  while (true) {
+    String input = stdin.readLineSync();
+    int day = int.parse(input);
+    if (day > 0 && day < 26) {
+      switch (day) {
+        default:
+          print('Day ${day} not yet implemented');
+          print('Please pick another!');
+      }
+    } else {
+      print('Between 1 and 25...');
+    }
+  }
 }
