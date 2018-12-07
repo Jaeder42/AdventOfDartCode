adbd0aafd6f38a81f167fad882243e2a0ba43a47
Johan Jäderblom <johan.jaderblom@valtech.se>
2018-12-07 09:24:34 +0100

    part1

--- lib/dayseven/day_seven.dart
+++ lib/dayseven/day_seven.dart
@@ -11,20 +11,24 @@ partone() async {
     var config = File('${current.path}/lib/dayseven/dayseven.txt');
     Stream<List<int>> inputStream = config.openRead();
     List<Step> steps = new List();
-
+    int c = "a".codeUnitAt(0);
+    int end = "z".codeUnitAt(0);
+    while (c <= end) {
+      steps.add(new Step(new String.fromCharCode(c).toUpperCase()));
+      c++;
+    }
     var lines = inputStream.transform(utf8.decoder).transform(LineSplitter());
     try {
       await for (var line in lines) {
-        Step tmp = new Step(line);
+        String id = line.split('')[36];
         Step existing =
-            steps.firstWhere((step) => step.id == tmp.id, orElse: () => null);
+            steps.firstWhere((step) => step.id == id, orElse: () => null);
         if (steps.length > 0 && existing != null) {
           existing.addPrerequesite(line);
-        } else {
-          steps.add(tmp);
         }
       }
-      print(steps);
+      steps.sort();
+      run(new List<String>(), steps);
     } catch (e) {
       print(e);
     }
@@ -33,24 +37,51 @@ partone() async {
   }
 }
 
-class Step {
-  String id;
+run(List<String> completed, List<Step> steps) {
+  if (steps.length < 1) {
+    print(completed.join(''));
+    return null;
+  }
+  Step toAdd =
+      steps.firstWhere((step) => step.prerequesites.length == 0, orElse: null);
+  if (toAdd == null) {
+    print(completed.join(''));
+    return null;
+  } else {
+    completed.add(toAdd.id);
+    steps.remove(toAdd);
+    removeCompleted(toAdd.id, steps);
+    return run(completed, steps);
+  }
+}
+
+removeCompleted(String completed, List<Step> steps) {
+  for (Step step in steps) {
+    step.prerequesites.remove(completed);
+  }
+}
+
+class Step extends Comparable {
+  final String id;
   List<String> prerequesites;
-  Step(String input) {
-    List<String> asArray = input.split('');
-    this.id = asArray[5];
+  Step(this.id) {
     prerequesites = new List();
-    prerequesites.add(asArray[36]);
   }
 
   addPrerequesite(String input) {
     List<String> asArray = input.split('');
-    prerequesites.add(asArray[36]);
+    prerequesites.add(asArray[5]);
   }
 
   @override
   String toString() {
     // TODO: implement toString
     return '$id : $prerequesites';
+  }
+
+  @override
+  int compareTo(other) {
+    // TODO: implement compareTo
+    return this.id.compareTo(other.id);
   }
 }
